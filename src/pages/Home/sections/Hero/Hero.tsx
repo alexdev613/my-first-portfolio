f44250ce064d7db55315c52baa90121ae96fc875
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-06 15:58:21 -0300

    fix: correction Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -6,18 +6,38 @@ import StyledButton from "../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
-
     const StyledHero = styled("div")(({ theme }) => ({
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
         display: "flex",
-        alignItems: "center"
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]: { // <=mobile 600-
+            // paddingTop: "100px",
+            height: "100vh",
+        },
+        [theme.breakpoints.up('sm')]: { // <=mobile 600+
+            paddingTop: "20px",
+            height: "120vh",
+        },
+        [theme.breakpoints.up('md')]: { // >=tablet 900+
+            paddingTop: "0",
+            height: "100vh",
+        }
     }))
 
     const StyledImg = styled("img")(({ theme }) => ({
-        width: "80%",
+        // width: "75%",
         borderRadius: "50%",
-        border: `1px solid ${theme.palette.primary.contrastText}`
+        border: `1px solid ${theme.palette.primary.contrastText}`,
+        [theme.breakpoints.up('xs')]: { // <=mobile 600-
+            width: "65%",
+        },
+        [theme.breakpoints.up('sm')]: { // <=mobile 600+
+            width: "70%",
+        },
+        [theme.breakpoints.up('md')]: { // >=tablet 900+
+            width: "75%",
+        }
     }))
 
     return (
@@ -25,7 +45,7 @@ const Hero = () => {
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={5}>
+                    <Grid item xs={12} md={4}>
                         <Box position="relative">
                             <Box position="absolute" width={"150%"} top={-100} right={0}>
                                 <AnimatedBackground />
@@ -35,7 +55,7 @@ const Hero = () => {
                             </Box>
                         </Box>
                     </Grid>
-                    <Grid item xs={12} md={7}>
+                    <Grid item xs={12} md={8}>
                         <Typography color="primary.contrastText" variant="h1" textAlign="center">Alex Heisenberg</Typography>
                         <Typography color="primary.contrastText" variant="h2" textAlign="center" pb={2}>I'm a Software Engineer</Typography>
                         <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
