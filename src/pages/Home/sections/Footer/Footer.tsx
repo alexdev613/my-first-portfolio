917677a803a3125f48d8dcb7b5a773ded2f2466d
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-19 15:22:41 -0300

    feat: creates Footer Component section and style file

---
+++ src/pages/Home/sections/Footer/Footer.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import {
+    FooterContainer,
+    FooterContent,
+    Column,
+    Title,
+    Menu,
+    MenuItem,
+    Link,
+    Image,
+    SocialsList,
+    SocialIcon,
+    IconLink,
+    Copyright,
+} from "./styles.ts";
+
+import { FaGithub, FaLinkedinIn, FaInstagram, FaYoutube } from "react-icons/fa";
+
+interface CustomLink {
+    label: string;
+    targetBlank?: boolean;
+    href: string;
+}
+
+export interface WidgetFooterMenu {
+    id: string;
+    title: string;
+    menus: CustomLink[];
+}
+
+const widgetMenus: WidgetFooterMenu[] = [
+    {
+        id: "1",
+        title: "Contato",
+        menus: [
+            { href: "#", label: "+55 (87) 98115-7269"},
+            { href: "#", label: "(11) 4109-6235"},
+        ],
+    },
+    {
+        id: "2",
+        title: "Localização",
+        menus: [
+            { href: "https://www.google.com.br/maps/place/Av.+Humberto+de+Alencar+Castelo+Branco+-+Alves+Dias,+S%C3%A3o+Bernardo+do+Campo+-+SP/@-23.7164415,-46.5885768,16.25z/data=!4m6!3m5!1s0x94ce43ee4d27dc6f:0x6617db7636183f88!8m2!3d-23.7163259!4d-46.5848744!16s%2Fg%2F1ymx0nh0w?entry=ttu", label: "Av. Humberto de Alencar Castelo Branco, S/N. Alves Dias, SBC - SP"},
+            { href: "https://www.google.com.br/maps/place/Av.+Paulista,+1754+-+189+-+Bela+Vista,+S%C3%A3o+Paulo+-+SP,+01310-200/@-23.5599133,-46.6593301,17z/data=!3m1!4b1!4m5!3m4!1s0x94ce59cc11b3304b:0x4e73dc20f448dede!8m2!3d-23.5599182!4d-46.6567552?entry=ttu", label: "Av. Paulista 1754 - conj 189"},
+            { href: "https://www.google.com.br/maps/place/Av.+Le%C3%A3o+Dourado,+680+-+Caiuca,+Caruaru+-+PE,+55034-190/@-8.2875222,-35.9884435,18.25z/data=!4m6!3m5!1s0x7a98b9a6a0dcdf3:0xe25a7edc815c9951!8m2!3d-8.2877213!4d-35.9876482!16s%2Fg%2F11j0tb2lm9?entry=ttu", label: "Av. Leão Dourado, S/N. Vila Kennedy, Caruaru - PE"},
+        ],
+    },
+    {
+        id: "3",
+        title: "Sobre Nós",
+        menus: [
+            { href: "#", label: "Condições Gerais" },
+            { href: "#", label: "Política de Privacidade" },
+            { href: "#", label: "Cookies" },
+        ],
+    },
+];
+
+const Footer: React.FC = () => {
+    const renderWidgetMenuItem = (menu: WidgetFooterMenu) => (
+        <Column key={menu.id}>
+            <Title>{menu.title}</Title>
+            <Menu>
+                {menu.menus.map((item, index) => (
+                    <MenuItem key={index}>
+                        <Link
+                          href={item.href}
+                          textDecoration="none"
+                          color="#fff"
+                          fontSize="inherit">
+                            {item.label}
+                        </Link>
+                    </MenuItem>
+                ))}
+            </Menu>
+            {/* Adicione a imagem apenas para o item "Sobre Nós" */}
+            {menu.title === "Sobre Nós" && (
+                <Image
+                src="src\assets\images\site-protegido.webp"
+                alt="Site Protegido"
+                style={{ width: "70%" }}/>
+            )}
+        </Column>
+    );
+
+    return (
+        <FooterContainer>
+            <FooterContent>
+                <Column>
+                    <Image src="src\assets\images\chemicalLogoAlex.PNG" alt="Logo Chemical Alex" />
+                    <SocialsList>
+
+                        <SocialIcon>
+                            <IconLink>
+                              <FaGithub />
+                            </IconLink>
+                        </SocialIcon>
+
+                        <SocialIcon>
+                            <IconLink>
+                              <FaLinkedinIn />
+                            </IconLink>
+                        </SocialIcon>
+
+                        <SocialIcon>
+                            <IconLink>
+                                <FaInstagram />
+                            </IconLink>
+                        </SocialIcon>
+
+                        <SocialIcon>
+                            <IconLink>
+                                <FaYoutube />
+                            </IconLink>
+                        </SocialIcon>
+                    </SocialsList>
+                </Column>
+                {widgetMenus.map(renderWidgetMenuItem)}
+            </FooterContent>
+            <Copyright>
+                <span>
+                    Copyright © 2009 - 2024 AlexHeisenberg Operating Company. Todos os direitos reservados.
+                    A marca nominativa AlexHeisenberg é uma marca registrada globalmente.
+                </span>
+                <p>
+                    O uso deste site está sujeito aos termos de uso expressos. Ao usar este
+                    site, você concorda em estar vinculado por estes
+                </p>
+                <Link
+                    href="#"
+                    textDecoration="underline"
+                    color="#FFF"
+                    fontSize="0.825rem"
+                >
+                        Termos de Serviço universal
+                </Link>
+            </Copyright>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Home/sections/Footer/styles.ts
@@ -0,0 +1,140 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  background-color: #232323;
+  color: #fff;
+  padding: 3.6rem 0;
+`;
+
+export const FooterContent = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Column = styled.div`
+  width: 100%;
+  padding: 0 1rem;
+  margin-bottom: 2rem;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 200px) {
+    width: 100%;
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+    text-align: center;
+  }
+
+  @media (min-width: 600px) {
+    width: 50%;
+  }
+
+  @media (min-width: 900px) {
+    width: 25%;
+    align-items: normal;
+    text-align: justify;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+export const Menu = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const MenuItem = styled.li`
+  margin-bottom: 0.5rem;
+  font-size: 0.8rem;
+`;
+
+export const Link = styled.a<{
+    textDecoration: string,
+    color: string,
+    fontSize: string
+  }>`
+    /* color: #fff; */
+    font-size: ${props => props.fontSize};
+    color: ${props => props.color};
+    text-decoration: ${props => props.textDecoration};
+    transition: color 0.3s;
+
+    &:hover {
+      color: #ccc;
+    }
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+`;
+
+export const SocialsList = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+
+    @media (max-width: 360px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    
+  }
+`;
+
+export const SocialIcon = styled.div`
+  width: 35px;
+  height: 35px;
+  padding-top: 0.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 35px;
+  background: #ddd;
+  margin: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background .3s linear;
+
+  &:hover {
+    background: #444;
+  }
+`;
+
+export const IconLink = styled.a`
+    color: #000;
+    font-size: 1.5rem;
+    transition: color .3s linear;
+
+    &:hover {
+        color: #fff;
+    }
+`;
+
+export const Copyright = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+
+    span {
+        color: #fff;
+        font-size: 0.925rem;
+        margin: 5px;
+        font-weight: 500;
+    }
+
+    p {
+      padding: 0 1.2rem;
+      color: #eee;
+      font-size: 0.825rem;
+    }
+`;
