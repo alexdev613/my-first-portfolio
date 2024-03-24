83ce6fd1316bf8c588ad6b94c93a3b903e27c393
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-24 16:34:01 -0300

    feat: imports necessaries import properties necessary for using swiper into the project's main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import Home from "./pages/Home/Home"
+import { register } from "swiper/element";
+
+register();
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade';
 
 const App = () => {
 
