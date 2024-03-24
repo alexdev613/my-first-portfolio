3343d3851aa9e4fecfd10c2a9e29f40dc5e1a86d
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-23 21:11:15 -0300

    style: changes style of WideCard Component

--- src/components/WideCard/styles.tsx
+++ src/components/WideCard/styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const WideCardContainer = styled.div`
     width: 95%;
-    min-width: 613px;
+    min-width: 380px;
     border: 1px solid #454545;
     border-radius: 8px;
     overflow: hidden;
