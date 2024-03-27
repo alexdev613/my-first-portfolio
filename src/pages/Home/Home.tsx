1d59c53f501bf86112ba094f08844889d744d9a8
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-26 22:21:32 -0300

    feat: init ProjectsSections section and add in Home page, and insert position: fixed in the NavBar component in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,16 +4,18 @@ import About from "./sections/About/About"
 import FAQSection from "./sections/FAQSection/FAQSection"
 import Footer from "./sections/Footer/Footer"
 import Hero from "./sections/Hero/Hero"
+import ProjectsSection from "./sections/ProjectsSection/ProjectsSection"
 import Skills from "./sections/Skills/Skills"
 
 const Home = () => {
 
     return (
       <>
-        <NavBar />
+        <NavBar position={"fixed"} />
         <Hero />
         <About />
         <Skills />
+        <ProjectsSection />
         <FAQSection />
         <Footer />
         <WhatsappIcon />
---
+++ src/pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -0,0 +1,38 @@
+import { Button, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const ProjectsSection = () => {
+    const projects = [
+        { id: 1, title: "Projeto 1", link: "/" },
+        { id: 2, title: "Projeto 2", link: "/" },
+        { id: 3, title: "Projeto 3", link: "/" },
+        { id: 4, title: "Projeto 4", link: "/" },
+        { id: 5, title: "Projeto 5", link: "/" },
+        { id: 6, title: "Projeto 6", link: "/" },
+        { id: 7, title: "Projeto 7", link: "/" },
+        { id: 8, title: "Projeto 8", link: "/" },
+        // Adicione mais projetos conforme necessário
+    ];
+
+    return (
+        <Grid container spacing={3} justifyContent="center" mt={0} pb={10} bgcolor={"beige"} >
+            <h1>Área dos Projetos (Em construção) aguarde ...</h1>
+            {projects.map((project) => (
+                <Grid item xs={10} key={project.id}>
+                    <Link to={project.link} style={{ textDecoration: "none" }}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            style={{ height: "60px" }}
+                        >
+                            {project.title}
+                        </Button>
+                    </Link>
+                </Grid>
+            ))}
+        </Grid>
+    );
+};
+
+export default ProjectsSection;
