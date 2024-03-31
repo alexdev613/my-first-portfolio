5faf7ff7878a5cb2e2c3b86d321dbf2006c92953
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-30 22:20:27 -0300

    style: changes on style of Header of the  Blog Page

--- src/components/BlogPageComponents/Header/styles.ts
+++ src/components/BlogPageComponents/Header/styles.ts
@@ -11,9 +11,17 @@ export const HeaderContainer = styled.header`
 
 export const Title = styled.h1`
   margin-top: 100px;
-  font-size: 4rem;
+  font-size: 4.125rem;
   line-height: 1rem;
   color: white;
+
+  @media (max-width: 405px) {
+    font-size: 3rem;
+  }
+
+  @media (max-width: 300px) {
+    font-size: 2.125rem;
+  }
 `;
 
 export const Subtitle = styled.h2`
@@ -21,4 +29,13 @@ export const Subtitle = styled.h2`
   color: white;
   font-size: 1.2rem;
   font-weight: lighter;
+  text-align: center;
+
+  @media (max-width: 405px) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: 300px) {
+    font-size: 0.75rem;
+  }
 `;
