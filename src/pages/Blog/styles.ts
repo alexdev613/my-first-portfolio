faf7822463a2f7ae8787fab59d6eab5856b28110
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-04-01 21:50:44 -0300

    style: style of Blog main page component

--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const BodyContainer = styled.body`
-    padding: 1.875rem;
     background-color: #232323;
     color: #fff;
     min-height: 80vh;
+`;
+
+export const ProvisoryContainer = styled.div`
+    padding: 1.875rem;
 `;
 
 export const MiniCardContainer = styled.div`
@@ -12,7 +15,7 @@ export const MiniCardContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
-    padding: 1rem;
+    padding: 1.875rem;
     column-gap: 1.5rem;
 
     @media (max-width: 900px) {
