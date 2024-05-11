3d9f5edeef46a2ebb661f9778cf6568583fbdd8c
Alex Nascimento <alexjose68@hotmail.com>
2024-05-10 21:50:37 -0300

    feat: some updates and refactoring

---
+++ src/components/BlogPageComponents/BlogCards/ColumnistCard/ColumnistCard.tsx
@@ -0,0 +1,9 @@
+const ColumnistCard = () => {
+  return (
+    <div>
+      <h1>Card do Coluna</h1>
+    </div>
+  )
+}
+
+export default ColumnistCard;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import { CssBaseline, ThemeProvider } from '@mui/material'
@@ -7,11 +6,9 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {/* <App /> */}
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </ThemeProvider>
 )
